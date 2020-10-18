2bbf22a4d67c52f9e4cb3e86b18728f124c0433d
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-18 14:23:39 +0530

    third commit

---
+++ Hobbies.java
@@ -0,0 +1,30 @@
+class Work
+{
+public static void main( String[]args)
+{
+System.out.println(" Hello");
+read();
+}
+public static void read()
+{
+System.out.println(" books");
+learn();
+}
+public static void learn()
+{
+System.out.println("new things");
+draw();
+}
+public static void draw()
+{
+System.out.println("sketch");
+mindrest();
+}
+public static void mindrest()
+{
+System.out.println("meditation");
+}
+}
+
+
+ 
