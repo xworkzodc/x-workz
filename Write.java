e0e9a504ec99f09958ac2c0dfd214b242dc5a014
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-18 15:31:25 +0530

    fourth commit

---
+++ Write.java
@@ -0,0 +1,15 @@
+class Main
+{
+public static void main(String[] ags)
+{
+System.out.println(" Hello");
+write();
+write();
+write();
+}
+public static void write()
+{
+System.out.println("books");
+}
+}
+
