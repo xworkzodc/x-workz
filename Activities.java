c5df5d9fa3eb55ab7557d44ca3e34e40818c9cca
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-18 14:17:09 +0530

    My second commit

---
+++ Activities.java
@@ -0,0 +1,18 @@
+class Activities
+{
+public static void main( String [] args)
+{
+System.out.println ("main");
+music();
+}
+public static void music(){
+System.out.println("headphones");
+drinking();
+}
+public static void drinking(){
+System.out.println("water");
+}
+}
+
+
+
