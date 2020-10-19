9bf357845bb85a2501044da57444222f98721476
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-19 23:25:14 +0530

    seventh commit

---
+++ Mobile.java
@@ -0,0 +1,24 @@
+public class Mobile
+{
+public static void main(String[] args)
+{
+byte consonents=21 ;
+System.out.println(consonents);
+short numberOfCountriesInTheWorld=195;
+System.out.println(numberOfCountriesInTheWorld);
+int numberOfAppsInPlayStore=3040000 ;
+System.out.println(numberOfAppsInPlayStore);
+long numberOfLongitudes=360;
+System.out.println(numberOfLongitudes);
+float accelerationOfGravity=9.8f; 
+System.out.println(accelerationOfGravity);
+double radiusOfSun=2.95325008;
+System.out.println(radiusOfSun);
+boolean oneDivideByZeroIsInfinity=false;
+System.out.println(oneDivideByZeroIsInfinity);
+char grade='D';
+System.out.println(grade);
+String animal="Rabbit";
+System.out.println(animal);
+}
+}
