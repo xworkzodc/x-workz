ee5cdb9e5981542ca1173e7ae9e7119bd3675fd8
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-19 23:27:20 +0530

    Eighth commit

---
+++ Plastic.java
@@ -0,0 +1,24 @@
+public class Plastic
+{
+public static void main(String[] args)
+{
+byte hoursInADay=24;
+System.out.println(hoursInADay);
+short numberOfMeridians=360;
+System.out.println(numberOfMeridians);
+int numberOfLanguagesSpokenInWorld=7117;
+System.out.println(numberOfLanguagesSpokenInWorld);
+long speedOfLight=299792458;
+System.out.println(speedOfLight);
+float plankConstant=6.62f; 
+System.out.println(plankConstant);
+double permittivityOfFreeSpace=8.8541878170000000;
+System.out.println(permittivityOfFreeSpace);
+boolean colourOfMangoIsBlue =false;
+System.out.println(colourOfMangoIsBlue);
+char grade='B';
+System.out.println(grade);
+String Fruit="Strawberry";
+System.out.println(Fruit);
+}
+}
