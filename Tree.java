019c1811b5e9ae3af319a6f1351fd98405b7ffe3
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-19 23:22:09 +0530

    fifth commit

---
+++ Tree.java
@@ -0,0 +1,32 @@
+public class Tree
+{
+public static void main(String[]para )
+{
+byte fingers =20;
+System.out.println(fingers);
+short daysInAYear =365;
+System.out.println(daysInAYear);
+int numberOfEngineersInIndia=1500000;
+System.out.println(numberOfEngineersInIndia);
+long populationOfIndia=1352642280;
+System.out.println(populationOfIndia);
+float percentageOfStudent=6.181f;
+System.out.println(percentageOfStudent);
+double chargeOfElectron=1.620217662000000000000;
+System.out.println(chargeOfElectron);
+boolean earthIsFlat=false;
+System.out.println(earthIsFlat);
+char grade='A';
+System.out.println(grade);
+String name="Sushma";
+System.out.println(name);
+}
+}
+
+
+
+
+
+
+
+
