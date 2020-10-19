2cc804927dd2cd42e108a202cfc4e4281f349a75
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-19 23:23:41 +0530

    sixth commit

---
+++ Book.java
@@ -0,0 +1,28 @@
+public class Book
+{
+public static void main(String[] args)
+{
+byte lettersInEnglish=26;
+System.out.println(lettersInEnglish);
+short statesOfKarnataka=29;
+System.out.println(statesOfKarnataka);
+int secondInADay=86400;
+System.out.println(secondInADay);
+long numberOfOrpansInIndia=29600000;
+System.out.println(numberOfOrpansInIndia);
+float valueOfRootThree=1.732f; 
+System.out.println(valueOfRootThree);
+double valueOfPi=3.141592653589793238;
+System.out.println(valueOfPi);
+boolean twoIsAPrimeNumber=true;
+System.out.println(twoIsAPrimeNumber);
+char grade='B';
+System.out.println(grade);
+String wish="Hello";
+System.out.println(wish);
+}
+}
+
+
+
+
