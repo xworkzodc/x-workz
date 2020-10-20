8241d74c4cfb42b58d14b8311a840f121b967864
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-20 18:36:02 +0530

    night commit

---
+++ paper.java
@@ -0,0 +1,22 @@
+public class Paper
+{
+public static void main(String[] c)
+{
+System.out.println("Sushma");
+printEmail("nyamagowdarsushma.xworkz@gmail.com");
+printProgrammingLanguage("java");
+}
+
+
+public static void printEmail(String email)
+{
+System.out.println("Email:" +email);
+}
+
+public static void printProgrammingLanguage(String programmingLangauge)
+{
+System.out.println("programmingLangauge:"+programmingLangauge);
+}
+
+}
+
