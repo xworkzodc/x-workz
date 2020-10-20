7e15a3bea8f131da4a58e5cf27cde3f8ff38d639
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-20 19:24:51 +0530

    tenth commit

---
+++ Pencil.java
@@ -0,0 +1,22 @@
+public class Pencil
+{
+public static void main(String[] sn)
+{
+System.out.println("Create");
+printAdharNumber(637653673667L);
+printPhoneNumber(9731221789L);
+printEmail("nyamagowdarsushma.xworkz@gmail.com");
+}
+public static void printAdharNumber(final long adharNumber )
+{
+System.out.println("adharNumber:" +adharNumber);
+}
+public static void printPhoneNumber(long phoneNumber)
+{
+System.out.println("phoneNumber:"+phoneNumber);
+}
+public static void printEmail(String email)
+{
+System.out.println("Email:" +email);
+}
+}
