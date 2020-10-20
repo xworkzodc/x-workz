0dadaaea25a7cb31e8656cdb25e5dfff9718339d
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-20 18:46:26 +0530

    tenth commit

---
+++ wish.java
@@ -0,0 +1,13 @@
+public class Wish
+{
+public static void main(String[] c)
+{
+System.out.println("Sushma");
+printWelcome();
+}
+public static void printWelcome()
+{
+
+System.out.println("WELCOME");
+}
+}
