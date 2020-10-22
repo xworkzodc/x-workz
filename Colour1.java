6fb17b307da2dec92eb8f41192e916572fef2ad2
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-22 22:31:37 +0530

    eleventh commit

---
+++ Colour1.java
@@ -0,0 +1,35 @@
+public class Colour1{	
+	public static void main(String[] politicians)
+	{
+		System.out.println("JVM invokes main");		
+		String[] statesArray={"Karnataka","Kerala","Jharkhand","Maharastra","Gujarat","Goa","Madhya Pradesh","Manipur","Odissa","Panjab","TamilNadu",
+		"Rajasthan","Telangana","Haryana","Assam"};
+		states(statesArray);	
+		
+		System.out.println("exit main ");
+	}		
+	public static void states(String[] states)
+	{
+		
+		System.out.println("states "+ states );		
+		int size=states.length;
+		System.out.println(size);
+		System.out.println("states @ index 0 "+states[0]);
+		System.out.println("states @ index 1 "+states[1]);
+		System.out.println("states @ index 2 "+states[2]);
+		System.out.println("states @ index 3 "+states[3]);
+		System.out.println("states @ index 4 "+states[4]);
+		System.out.println("states @ index 5 "+states[5]);
+		System.out.println("states @ index 6 "+states[6]);
+		System.out.println("states @ index 7 "+states[7]);
+		System.out.println("states @ index 8 "+states[8]);
+		System.out.println("states @ index 9 "+states[9]);
+		System.out.println("states @ index 10 "+states[10]);
+		System.out.println("states @ index 11 "+states[11]);
+		System.out.println("states @ index 12 "+states[12]);
+		System.out.println("states @ index 13"+states[13]);
+		System.out.println("states @ index 14 "+states[14]);
+	}
+	
+	
+}
